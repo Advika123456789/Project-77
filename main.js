80a2ecc5b8245dc87e33d22e950b73e9c5999a30
Advika123456789 <78788299+Advika123456789@users.noreply.github.com>
2021-07-22 13:50:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var first_name="Ranbir";
+var second_name="Kapoor";
+
+var fullname=first_name.concat(second_name);
+
+console.log(first_name);
+console.log(second_name);
+console.log(fullname);
+
+var a=5;
+var b=10;
+
+var add=a+b;
+console.log(add);
+
+var minus= b - a;
+console.log(minus);
+
+var multi= a * b;
+console.log(multi);
+
+var divi=b / a;
+console.log(divi);
